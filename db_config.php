29215ffe1710f95dcf6a99669b0073c0ded97333
kemallio <D20221101856@siswa.upsi.edu.my>
2024-12-26 13:07:09 +0800

    db connection for title

---
+++ db_config.php
@@ -0,0 +1,22 @@
+<?php
+// db_config.php
+
+// Enable detailed error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Database connection parameters
+$servername = "localhost";
+$username = "d20221101856";
+$password = "Aa151k027!!";
+$dbname = "d20221101856";
+
+// Create a new MySQLi connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- index.php
+++ index.php
@@ -1,33 +1,45 @@
+<?php
+// index.php
+
+// Include the database configuration file
+require_once 'db_config.php';
+
+// Fetch data from the database
+$sql = "SELECT name, description FROM website_desc LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $website_name = $row['name'];
+    $website_desc = $row['description'];
+} else {
+    $website_name = "Default Name";
+    $website_desc = "Default Description";
+}
+
+// Close the database connection
+$conn->close();
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <link rel="stylesheet" href="styles-framework.css">
 </head>
-
 <body>
-
     <div class="card">
         <div class="header">
-            <h1>Syahmi Akmal</h1>
-            <p>student of Software Engineering (Educational Software)</p>
+            <h1><?php echo htmlspecialchars($website_name); ?></h1>
+            <p><?php echo htmlspecialchars($website_desc); ?></p>
         </div>
-
         <div class="topnav">
             <a href="#">Home</a>
             <a href="#" style="float:right">Link</a>
         </div>
     </div>
-
     <div class="row">
-
         <?php include 'left-column.php'; ?>
         <?php include 'right-column.php'; ?>
-
     </div>
-
     <?php include 'footer.php'; ?>
-
 </body>
-
 </html>
