1ee71c82d7189334e7225cf516b8e00ae6c44b53
kemallio <D20221101856@siswa.upsi.edu.my>
2024-12-27 10:27:20 +0800

    refix

--- index.php
+++ index.php
@@ -19,31 +19,13 @@ include 'layouts/header.php';
             <p><?php echo htmlspecialchars($website_desc); ?></p>
         </div>
         <div class="topnav">
-            <a href="#" onclick="showTab('home')">Home</a>
-            <a href="#" onclick="showTab('bmi')" style="float:right">BMI Calculator</a>
+            <a href="#">Home</a>
+            <a href="#">BMI Calculator</a>
             <a href="#" style="float:right">Link</a>
         </div>
     </div>
-    
-    <div class="content">
-        <!-- Home Section -->
-        <div id="home" class="tab-content">
-            <h2>Welcome to the Home Page</h2>
-            <p>This is the homepage content. Click "BMI Calculator" to switch tabs.</p>
-        </div>
+    <?php include 'layouts/bmi.php'; ?>
 
-        <!-- BMI Calculator Section -->
-        <div id="bmi" class="tab-content" style="display: none;">
-            <div class="row">
-                <div class="leftcolumn">
-                    <?php include 'layouts/left-column.php'; ?>
-                </div>
-                <div class="rightcolumn">
-                    <?php include 'layouts/right-column.php'; ?>
-                </div>
-            </div>
-        </div>
-    </div>
     <?php include 'layouts/footer.php'; ?>
 </body>
 
